0c4e67043e50437a9961e2b79fbedb91fbe2fc3d
ProjetoDelimeterSite <ProjetoDelimeterSite@gmail.com>
2025-06-30 22:49:36 +0000

    feat: Adicionar rota raiz para redirecionamento à página inicial no DelimeterRoutes

--- src/Routes/DelimeterRoutes.php
+++ src/Routes/DelimeterRoutes.php
@@ -6,6 +6,7 @@ use Htdocs\Src\Controllers\DelimeterController;
 class DelimeterRoutes {
     public function __construct($route) {
         $delimeterController = new DelimeterController();
+        $route->add('GET', '/', [$delimeterController, 'mostrarHome']);
         $route->add('GET', '/delimeter', [$delimeterController, 'mostrarHome']);
         $route->add('GET', '/delimeter/calculo', [$delimeterController, 'mostrarCalculo']);
         $route->add('GET', '/delimeter/sobre', [$delimeterController, 'mostrarSobre']);
