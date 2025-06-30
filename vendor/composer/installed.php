568f420c3208af806f35e69022f680afa07ec1d8
ProjetoDelimeterSite <ProjetoDelimeterSite@gmail.com>
2025-06-30 20:50:04 +0000

    fix: Atualizar referência do pacote para a versão correta

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'htdocs/src',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'eda849d80401f5f263d582acd3dfa500c22020c9',
+        'reference' => '1beb81959b751867593e86a03ce5f80996d8ab94',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'htdocs/src' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'eda849d80401f5f263d582acd3dfa500c22020c9',
+            'reference' => '1beb81959b751867593e86a03ce5f80996d8ab94',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
